0bcc6ef76ec607f381bbcaaeaf5ecb4048e58d89
Bryan Urbina Guevara <bryanguevara38@gmail.com>
2023-01-30 23:06:49 -0600

    solo se puede trabajar el formato 02, falta modificar ancho otros formatos

--- main.js
+++ main.js
@@ -18,8 +18,21 @@ let info = {
 }
 
 $(function () {
-    let img = ['img/f1.jpeg', 'img/f2.jpeg', 'img/f3.jpeg', 'img/f4.jpeg']
+    let img = ['img/f1.jpeg', 'img/f2.jpeg', 'img/f3.jpeg', 'img/f4.jpg']
     let container = $('#format') // Selector del contenedor donde se pondra la informacion
+
+    $("#download").on('click', function () {
+        html2canvas(container, {
+            allowTaint: true,
+            onrendered: function (canvas) {
+                var imageData = canvas.toDataURL("image/jpeg", '1.0');
+                var newData = imageData.replace(/^data:image\/jpg/,
+                    "data:application/octet-stream");
+                $("#download").attr("download", "image.jpg").attr("href", newData);
+            }
+        });
+
+    });
 
     const alert = (title, message) => {
         Swal.fire({
@@ -29,11 +42,44 @@ $(function () {
         })
     }
 
+
+    //Genera las previsualizaciones de las imagenes de la persona y la firma, donde se encuentra la imagen de la cedula
+    const createPreview = (person, signature, formato) => {
+        let imgPerson = $('#container-img-person');
+        let imgSignature = $('#container-img-signature');
+
+        imgPerson.attr('src', `${person}`).addClass(`${'img-person-'+formato}`);
+        imgSignature.attr('src', `${signature}`).addClass(`${'img-signature-'+formato}`)
+        // imgSignature.css('backgroundImage', `url(${signature})`)
+    }
+
+    // Function for codifier the images to push into container 'format'
+    const urlsImages = (formato) => {
+        let person = $('#imgPerson')[0].files;
+        let signature = $('#imgSignature')[0].files;
+
+        if (person.length === 0 && signature.length === 0) {
+            alert('Error de seleccion', 'Seleccione las imagenes correspondientes')
+        } else if (person.length === 0 && signature.length === 1) {
+            alert('Error de seleccion', 'Seleccione la imagen de la persona')
+        } else if (person.length === 1 && signature.length === 0) {
+            alert('Error de seleccion', 'Seleccione la imagen de la firma')
+        } else {
+            let urlPerson = URL.createObjectURL(person[0])
+            let urlSignature = URL.createObjectURL(signature[0])
+            createPreview(urlPerson, urlSignature, formato);
+        }
+    }
+
     // Funcion para hacer el consilado final parte tracera con los caracteres
-    const consolidated = (checkId, formato) => {
+    const consolidated = (checkId) => {
         let s11 = '<<<<<<<<<<<'; // 11 unidades
         let s15 = '<<<<<<<<<<<<<<<'; // 15 unidades
         let sp = '';
+
+        // Parte para preparar el luglar dnd estaran los caracteres
+        let containerIc = $('#container-ic');
+        containerIc.attr('class', `${'container-ic-'+checkId}`)
 
         //Espacio para el IC1
         let ced = $('#cedula').val();
@@ -43,14 +89,15 @@ $(function () {
 
         // Espacio para el IC2
         let sex = $('#sexo').val();
-        let birthday = $('#expiracion').val();
+        let birthday = $('#fecha_nacimiento').val();
         let expiration = $('#expiracion').val();
         let arrBirthday = birthday.split('-'); //arrBirthdayday
         let arrExp = expiration.split('-'); //Expiraion
-        let yearBirthday = arrBirthday[2].split('');
-        let yearExp = arrExp[2].split('');
+        let yearBirthday = arrBirthday[0].split('');
+        let yearExp = arrExp[0].split('');
 
-        let ic2 = yearBirthday[2][3] + arrBirthday[2] + arrBirthday[1] + '6' + sex + yearExp[2][3] + arrExp[2] + arrExp[1] + '9NIC' + s11 + '8';
+        let ic2 = yearBirthday[2] + yearBirthday[3] + arrBirthday[1] + arrBirthday[2] + '6' + sex + yearExp[2] + yearExp[3] + arrExp[1] + arrExp[2] + '9NIC' + s11 + '8';
+
 
         // ESPACIO PARA REALIZA EL IC3
 
@@ -71,7 +118,7 @@ $(function () {
                 var x = s[a].charAt(i);
 
                 if (!number) {
-                    sp += `<span class="${checkId} ${checkId +'-number'}">${x}</span>`;
+                    sp += `<span class="${checkId}  ${checkId +'-number'}">${x}</span>`;
                     continue;
                 }
                 sp += `<span class="${checkId}">${x}</span>`
@@ -80,11 +127,12 @@ $(function () {
             sp = '';
         }
         container.append(sp)
+        urlsImages(checkId);
     }
 
     // Funcion para recolectar la informacion ingresada en los inputs
 
-    const recollect = (checkId, formato) => {
+    const recollect = (checkId) => {
         var sp = '';
 
         $.each(info, v => {
@@ -101,7 +149,7 @@ $(function () {
         })
 
         container.append(sp);
-        consolidated(checkId, formato)
+        consolidated(checkId)
     }
 
     // parte en donde hacemos la descarga
@@ -147,67 +195,54 @@ $(function () {
         } else if (v === 0) {
             alert('Error de seleccion', 'Por favor, selecciona un formato');
         } else {
-            let arrayFormato = inputId.split('');
-            let formato = arrayFormato[1] + arrayFormato[2];
-            let data = {
-                'formato': formato,
-                'checkId': inputId
+            // let arrayFormato = inputId.split('');
+            // let formato = arrayFormato[1] + arrayFormato[2];
+            // let data = {
+            //     'formato': formato,
+            //     'checkId': inputId
+            // }
+            // recollect(data.checkId, data.formato);
+            if (inputId === 'f02') {
+                container.attr('width', '450px');
+            } else if (inputId === 'f04') {
+                container.attr('width', '414px');
             }
-            recollect(data.checkId, data.formato);
+            recollect(inputId);
         }
-    }
-
-    // Function for codifier the images to push into container 'format'
-    const urlsImages = () => {
-        let person = $('#imgPerson')[0].files;
-        let signature = $('#imgSignature')[0].files;
-
-        if (person.length === 0 && signature.length === 0) {
-            alert('Error de seleccion', 'Seleccione las imagenes correspondientes')
-        } else if (person.length === 0 && signature.length === 1) {
-            alert('Error de seleccion', 'Seleccione la imagen de la persona')
-        } else if (person.length === 1 && signature.length === 0) {
-            alert('Error de seleccion', 'Seleccione la imagen de la firma')
-        } else {
-            let urlPerson = URL.createObjectURL(person[0])
-            let urlSignature = URL.createObjectURL(signature[0])
-            createPreview(urlPerson, urlSignature);
-        }
-    }
-
-    //Genera las previsualizaciones de las imagenes de la persona y la firma, donde se encuentra la imagen de la cedula
-    function createPreview(person, signature) {
-        let imgPerson = $('#container-img-person')
-        let imgSignature = $('#container-img-signature')
-        let check = validateCheckbox();
-
-        imgPerson.attr('src', `${person}`)
-        imgSignature.attr('src', `${signature}`)
-        // imgSignature.css('backgroundImage', `url(${signature})`)
     }
 
     // Funcion para mostrar en el modal la vista previa del formato seleccionado o a hacer
-    /*$('input[type="checkbox"]').on('change', function (e) {
+    $('input[type="checkbox"]').on('change', function (e) {
 
         if ($(this).is(':checked')) {
             let position = $(this).attr('data-image');
-            let containerImg = $('#img-ced')
-            containerImg.attr('src', img[position])
-            $('#myModal').modal('show');
+            let containerImg = $('#format')
+            containerImg.css('backgroundImage', `url(${img[position]})`)
+            // $('#myModal').modal('show');
 
         } else {
             // Hacer algo si el checkbox ha sido deseleccionado
             console.log("El checkbox con valor " + $(this).val() + " ha sido deseleccionado");
         }
-    });*/
+    });
 
-    
+
     // BTN PARA VISUALIZAR LA INFORMACION INSERTADA
     $('#visualize').on('click', function () {
         validateCheckbox();
     })
     $('#cli').on('click', function () {
-        console.log(validateCheckbox());
+        let s11 = '<<<<<<<<<<<'; // 11 unidades
+
+        let birthday = $('#fecha_nacimiento').val();
+        let expiration = $('#expiracion').val();
+        let arrBirthday = birthday.split('-'); //arrBirthdayday
+        let arrExp = expiration.split('-'); //Expiraion
+        let yearBirthday = arrBirthday[0].split('');
+        let yearExp = arrExp[0].split('');
+
+        let ic2 = yearBirthday[2] + yearBirthday[3] + arrBirthday[1] + arrBirthday[2] + '6' + 'M' + yearExp[2] + yearExp[3] + arrExp[1] + arrExp[2] + '9NIC' + s11 + '8';
+        console.log(ic2)
     })
 
 })
